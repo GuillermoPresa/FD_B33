1e93bdffd280a10c3073b406cddf14b6d461d999
frederikcde <61149611+frederikcde@users.noreply.github.com>
2020-03-09 15:10:07 +0100

    Create Data_processing_flip.py

---
+++ Data_processing_flip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 15:04:57 2020
+
+@author: frederik
+"""
+
+###### Data Processing tasks ########
+
+#task 1: First Stationary Measurement Series
