2ada6c8a4c76f914dcd18cc5da498a45d05d4e19
bobvdheijden99 <bobvdheijden99@outlook.com>
2020-03-10 14:53:47 +0100

    All formulas on page 9 are now there

--- aero_coeff.py
+++ aero_coeff.py
@@ -7,6 +7,18 @@ def CAStomach(press0, press, density0, cas, gamma = 1.4):
     a = 1/(gamma-1)
     return sqrt(2*a*((1 + ((2*gamma*a)**-1)*(density0/press0)*cas**2)**((gamma*a)**-1)-1))
 
-def CalibratedPressure(SeaLevelPressure, SeaLevelTemperature, PressureAltitude,):
-	return 
+def SpeedOfSound(T,gamma = 1.4, R = 287.1):
+    T = T + 273.15
+    a = sqrt(gamma * R * T)
+    return a
+
+def StaticAirTemperature(Mach, TotalTemp, gamma = 1.4):
+    return TotalTemp / (1 + ((gamma - 1)/2) * Mach)
+
+def TAS(Mach, T):
+    a = SpeedOfSound(T)
+    return Mach * a
+
+def CalibratedPressure(SeaLevelPressure, SeaLevelTemperature, PressureAltitude, LapseRate, R = 287.1, g0 = 9.80665):
+	return SeaLevelPressure * (1 + LapseRate * PressureAltitude / SeaLevelTemperature)**(-g0/(LapseRate*R))
 
