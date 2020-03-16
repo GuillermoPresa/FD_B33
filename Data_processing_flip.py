292238483e1a2ce44cf8f6499e3fcd994afd9688
frederikcde <61149611+frederikcde@users.noreply.github.com>
2020-03-16 18:18:17 +0100

    Update Data_processing_flip.py

--- Data_processing_flip.py
+++ Data_processing_flip.py
@@ -14,36 +14,46 @@ import matplotlib.pyplot as plt
 from cg_pos import cg, payload_list
 import ISA_calculator
 import Data_reader
+from Main import Xcg1, Xcg2, CN
 
 #### task 1: First Stationary Measurement Series
 
-# General inputs
-gamma = 1.4                                     # [-]
-lam = -0.0065                                   # [-]
-Ws = 60500                                      # [N] Reduced aircraft weight from assingment
-R = 287.0                                       # [J/(mol*K)] Specific gas Constant of Air
+# Aircraft inputs
+S = 30.00                                               # [m^2] Wing Surface Area
+c_bar = 2.0569                                          # [m] Average Chord Length
+
+# Envirnonment inputs
+gamma = 1.4                                             # [-]
+lam = -0.0065                                           # [-]
+Ws = 60500                                              # [N] Reduced aircraft weight from assingment
+R = 287.0                                               # [J/(mol*K)] Specific gas Constant of Air
 
 # Sea level conditions
-p0 = ISA_calculator.pressure0                   # [N/m^2] Pressure at Sea level
-g0 = ISA_calculator.g0                          # [m/s^2] Gravitational Constant
-rho_0 = ISA_calculator.rho0                     # [kg/m^3] Air Density at Sea level
-T0 = ISA_calculator.tempn[0]                    # [K] Temperature at Sea level
+p0 = ISA_calculator.pressure0                           # [N/m^2] Pressure at Sea level
+g0 = ISA_calculator.g0                                  # [m/s^2] Gravitational Constant
+rho_0 = ISA_calculator.rho0                             # [kg/m^3] Air Density at Sea level
+T0 = ISA_calculator.tempn[0]                            # [K] Temperature at Sea level
 
 # Inputs from the collected data
 Tm = Data_reader.flightdata['Dadc1_tat']                # [K] Measured Temperature
 Vc = Data_reader.flightdata['Dadc1_tas']                # [m/s] Measured Airspeed                                               
 hp = Data_reader.flightdata['Dadc1_alt']                # [N/m^2] Pressure altitude
-delta_meas = Data_reader.flightdata['elevator_dte']     # [deg] Measured Elevator Deflection
+el_def_meas = Data_reader.flightdata['elevator_dte']     # [deg] Measured Elevator Deflection
 AOA = Data_reader.flightdata['vane_AOA']                # [deg] Measured Angle of attack
 Fe = Data_reader.flightdata['column_fe']                # [N] Measured elevator control force
+time = Data_reader.flightdata['time']                   # [s] Time
 
+AFM = np.zeros(len(Data_reader.flightdata['Dadc1_tas'])) #still needs to be calculated 
+
+#thrust calculations
+#T =        from thrust.exe
 
 Ve_bar_lst = np.zeros(len(Data_reader.flightdata['Dadc1_tas']))
 red_el_def_lst = np.zeros(len(Data_reader.flightdata['elevator_dte']))
 red_Fe_lst = np.zeros(len(Data_reader.flightdata['column_fe']))
 
 
-def red_airspeed(hp, Vc, Tm):
+def red_airspeed(hp, Vc, Tm, AFM):
     # alt = altitude
     # hp = measured pressure altitude 
     # Vc = calibrated airspeed on airplane airspeed indicator
@@ -64,14 +74,14 @@ def red_airspeed(hp, Vc, Tm):
     Ve = Vt * m.sqrt(rho/rho_0)
     
     #calculation for the reduction of the equivalent airspeed
-    m_tot = cg(100/2.2, payload_list)[1]
+    m_tot = cg(AFM, payload_list)[1]
     W = m_tot * g0
     Ve_bar = Ve * m.sqrt(Ws/W)
     
-    return Ve, Ve_bar
+    return Ve, Ve_bar, rho, W
 
 
-def red_thrust(delta_meas):
+def red_thrust(el_def_meas):
     #reduction of the non-standard engine thrust
     # delta_meas = measured elevator deflection
     # red_el_def = reduced elevator deflection
@@ -81,23 +91,52 @@ def red_thrust(delta_meas):
     Tcs = 0             # [-] Standard thrust coefficient
     Tc = 0              # [-] Thrust coefficient
 
-    red_el_def = delta_meas - 1/Cmd * Cmtc * (Tcs -Tc)
+    red_el_def = el_def_meas - 1/Cmd * Cmtc * (Tcs -Tc)
     
     return red_el_def
 
 
-def red_force(Fe):
+def red_force(Fe, hp, Vc, Tm):
     #calculations for the reduced elevator control force
     # Fe = measured elevator control force
     # red_Fe = reduced elevator control force
     
-    #Aircraft weight calculations
-    m_tot = cg(100/2.2, payload_list)[1]
-    W = m_tot * g0
-    
-    red_Fe = Fe * Ws/W
+    red_Fe = Fe * Ws/(red_airspeed(hp, Vc, Tm)[3])
     
     return red_Fe
+
+
+def CL(hp, Vc, Tm, AFM):
+    #calculations for the lift coefficient
+    Cl = (red_airspeed(hp, Vc, Tm, AFM)[3])/(0.5 * red_airspeed(hp, Vc, Tm, AFM)[2] * red_airspeed(hp, Vc, Tm, AFM)[1] ** 2 * S)
+    
+    return Cl
+
+
+def CD(T, hp, Vc, Tm, AFM):
+    #calculations for the drag coefficient
+    
+    Cd = T/(0.5 * red_airspeed(hp, Vc, Tm, AFM)[2] * red_airspeed(hp, Vc, Tm, AFM)[1] ** 2 * S)
+
+    return Cd   
+
+
+def C_md(time1, time2):
+    
+    abs_difference1 = lambda list_value : abs(list_value - time1)
+    closest1 = min(time, key=abs_difference1)
+    index1 = time.index(closest1)
+    
+    abs_difference2 = lambda list_value : abs(list_value - time2)
+    closest2 = min(time, key=abs_difference2) 
+    index2 = time.index(closest2)
+    
+    el_def1 = el_def_meas[index1]
+    el_def2 = el_def_meas[index2]
+    
+    C_md = - 1/(el_def2 - el_def1) * CN * (Xcg2 - Xcg1)/c_bar
+    
+    return C_md
 
 
 def plotter(Ve_bar, el_def, F_e, alpha, CL, CD):
@@ -133,9 +172,9 @@ def plotter(Ve_bar, el_def, F_e, alpha, CL, CD):
 
 
 for i in range(0, len(Data_reader.flightdata['Dadc1_tas'])):
-    Ve_bar_lst[i] = red_airspeed(hp[i], Vc[i], Tm[i])[1]
-    red_el_def_lst[i] = red_thrust(delta_meas[i])
-    red_Fe_lst[i] = red_force(Fe[i])
+    Ve_bar_lst[i] = red_airspeed(hp[i], Vc[i], Tm[i], AFM[i])[1]
+    red_el_def_lst[i] = red_thrust(el_def_meas[i])
+    red_Fe_lst[i] = red_force(Fe[i], hp[i], Vc[i], Tm[i], AFM[i])
 
 
 
